d822729fbde777c65f641fdb8e56a18209fd44dd
manuelguanipa <manu_9124@hotmail.com>
2018-05-06 09:11:24 +0000

    Add files via upload

---
+++ detectnet-camera.cpp
@@ -0,0 +1,430 @@
+/*
+ * Copyright (c) 2017, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include "gstCamera.h"
+
+#include "glDisplay.h"
+#include "glTexture.h"
+
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+#include "cudaMappedMemory.h"
+#include "cudaNormalize.h"
+#include "cudaFont.h"
+
+#include "detectNet.h"
+
+#include <string.h>
+#include <wiringSerial.h>
+
+
+#define DEFAULT_CAMERA 1//-1	// -1 for onboard camera, or change to index of /dev/video V4L2 camera (>=0)	
+		
+//char serialPortFilename[] = "/dev/ttyACM0";//////////////////////////////
+
+
+
+bool signal_recieved = false;
+
+void sig_handler(int signo)
+{
+	if( signo == SIGINT )
+	{
+		printf("received SIGINT\n");
+		signal_recieved = true;
+	}
+}
+
+//int fd = serialOpen ("/dev/ttyACM0", 9600);
+int main( int argc, char** argv )
+{
+//////////////////////////////////////////////////
+/*
+ serialib LS;                                                            // Object of the serialib class
+    int Ret;                                                                // Used for return values
+    char Buffer[128];
+
+
+
+
+    // Open serial port
+
+    Ret=LS.Open(/dev/ttyACM0,9600);                                        // Open serial link at 115200 bauds
+    if (Ret!=1) {                                                           // If an error occured...
+        printf ("Error while opening port. Permission problem ?\n");        // ... display a message ...
+        return Ret;                                                         // ... quit the application
+    }
+    printf ("Serial port opened successfully !\n");
+
+
+
+
+    // Write the AT command on the serial port
+
+    Ret=LS.WriteString("1\n");                                             // Send the command on the serial port
+    if (Ret!=1) {                                                           // If the writting operation failed ...
+        printf ("Error while writing data\n");                              // ... display a message ...
+        return Ret;                                                         // ... quit the application.
+    }
+    printf ("Write operation is successful \n")
+LS.Close();
+*/
+
+//int fd = serialOpen ("/dev/ttyACM0", 9600);
+//serialClose(fd);
+/*
+if (fd < 0)
+	{
+		printf("ERROR");	
+		return 0;
+	}
+*/
+//serialPutchar (fd, 0);
+//serialPutchar (fd, 100);
+//serialPutchar (fd, 1);
+//serialPutchar (fd, 100);
+//serialClose(fd);
+/*
+	FILE *serPort = fopen(serialPortFilename, "w");////////////////////////////////////
+	if (serPort == NULL)
+	{
+		printf("ERROR");	
+		return 0;
+	}
+	int writeBuffer[1];
+	int midFaceY;
+	int midFaceX;
+	
+	int tiltChannel = 0;
+	writeBuffer[0]=tiltChannel;
+	fwrite(writeBuffer, 1, 1, serPort);
+
+	
+	int servoTiltPosition  = 90;
+	writeBuffer[0]=servoTiltPosition;
+	fwrite(writeBuffer, 1, 1, serPort);
+
+	int panChannel = 1;
+	writeBuffer[0]=panChannel;
+	fwrite(writeBuffer, 1, 1, serPort);
+
+	int servoPanPosition = 90;
+	writeBuffer[0]=servoPanPosition;
+	fwrite(writeBuffer, 1, 1, serPort);
+*/
+////////////////////////////////////////
+
+	printf("detectnet-camera\n  args (%i):  ", argc);
+
+	for( int i=0; i < argc; i++ )
+		printf("%i [%s]  ", i, argv[i]);
+		
+	printf("\n\n");
+	
+
+	/*
+	 * parse network type from CLI arguments
+	 */
+	/*detectNet::NetworkType networkType = detectNet::PEDNET_MULTI;
+
+	if( argc > 1 )
+	{
+		if( strcmp(argv[1], "multiped") == 0 || strcmp(argv[1], "pednet") == 0 || strcmp(argv[1], "multiped-500") == 0 )
+			networkType = detectNet::PEDNET_MULTI;
+		else if( strcmp(argv[1], "ped-100") == 0 )
+			networkType = detectNet::PEDNET;
+		else if( strcmp(argv[1], "facenet") == 0 || strcmp(argv[1], "facenet-120") == 0 || strcmp(argv[1], "face-120") == 0 )
+			networkType = detectNet::FACENET;
+	}*/
+	
+	if( signal(SIGINT, sig_handler) == SIG_ERR )
+		printf("\ncan't catch SIGINT\n");
+
+
+	/*
+	 * create the camera device
+	 */
+	gstCamera* camera = gstCamera::Create(DEFAULT_CAMERA);
+	
+	if( !camera )
+	{
+		printf("\ndetectnet-camera:  failed to initialize video device\n");
+		return 0;
+	}
+	
+	printf("\ndetectnet-camera:  successfully initialized video device\n");
+	printf("    width:  %u\n", camera->GetWidth());
+	printf("   height:  %u\n", camera->GetHeight());
+	printf("    depth:  %u (bpp)\n\n", camera->GetPixelDepth());
+	//printf("   shupalo\n\n");//////////////////////////////////////////////////////////////////
+
+	/*
+	 * create detectNet
+	 */
+	detectNet* net = detectNet::Create(argc, argv);
+	
+	if( !net )
+	{
+		printf("detectnet-camera:   failed to initialize imageNet\n");
+		return 0;
+	}
+
+
+	/*
+	 * allocate memory for output bounding boxes and class confidence
+	 */
+	const uint32_t maxBoxes = net->GetMaxBoundingBoxes();		printf("maximum bounding boxes:  %u\n", maxBoxes);
+	const uint32_t classes  = net->GetNumClasses();
+	
+	float* bbCPU    = NULL;
+	float* bbCUDA   = NULL;
+	float* confCPU  = NULL;
+	float* confCUDA = NULL;
+	
+	if( !cudaAllocMapped((void**)&bbCPU, (void**)&bbCUDA, maxBoxes * sizeof(float4)) ||
+	    !cudaAllocMapped((void**)&confCPU, (void**)&confCUDA, maxBoxes * classes * sizeof(float)) )
+	{
+		printf("detectnet-console:  failed to alloc output memory\n");
+		return 0;
+	}
+	
+
+	/*
+	 * create openGL window
+	 */
+	glDisplay* display = glDisplay::Create();
+	glTexture* texture = NULL;
+	
+	if( !display ) {
+		printf("\ndetectnet-camera:  failed to create openGL display\n");
+	}
+	else
+	{
+		texture = glTexture::Create(camera->GetWidth(), camera->GetHeight(), GL_RGBA32F_ARB/*GL_RGBA8*/);
+
+		if( !texture )
+			printf("detectnet-camera:  failed to create openGL texture\n");
+	}
+	
+	
+	/*
+	 * create font
+	 */
+	cudaFont* font = cudaFont::Create();
+	
+
+	/*
+	 * start streaming
+	 */
+	if( !camera->Open() )
+	{
+		printf("\ndetectnet-camera:  failed to open camera for streaming\n");
+		return 0;
+	}
+	
+	printf("\ndetectnet-camera:  camera open for streaming\n");
+	
+	
+
+
+
+
+
+	/*
+	 * processing loop
+	 */
+	float confidence = 0.0f;
+	
+	while( !signal_recieved )
+	{
+			
+		void* imgCPU  = NULL;
+		void* imgCUDA = NULL;
+		
+		//////////////////////////
+		 usleep(2);
+/////////////////////////////////////////////////////
+		// get the latest frame
+		if( !camera->Capture(&imgCPU, &imgCUDA, 1000) )
+			printf("\ndetectnet-camera:  failed to capture frame\n");
+
+		// convert from YUV to RGBA
+		void* imgRGBA = NULL;
+		
+		if( !camera->ConvertRGBA(imgCUDA, &imgRGBA) )
+			printf("detectnet-camera:  failed to convert from NV12 to RGBA\n");
+
+		// classify image with detectNet
+		int numBoundingBoxes = maxBoxes;
+	
+		if( net->Detect((float*)imgRGBA, camera->GetWidth(), camera->GetHeight(), bbCPU, &numBoundingBoxes, confCPU))
+		{
+			//printf("%i bounding boxes detected\n", numBoundingBoxes);
+			//printf("   shupalo\n\n");//////////////////////////////////////////
+			int lastClass = 0;
+			int lastStart = 0;
+			
+			for( int n=0; n < numBoundingBoxes; n++ )
+			{
+				const int nc = confCPU[n*2+1];
+				float* bb = bbCPU + (n * 4);
+				
+				//printf("bounding box %i   (%f, %f)  (%f, %f)  w=%f  h=%f\n", n, bb[0], bb[1], bb[2], bb[3], bb[2] - bb[0], bb[3] - bb[1]); 
+				/*
+				midFaceY= (bb[3] - bb[1])/2;
+				midFaceX= (bb[2] - bb[0])/2;
+
+				
+
+				if(midFaceY < (120 - 10))
+				{
+					if(servoTiltPosition >= 5)
+					{
+						servoTiltPosition=servoTiltPosition-1;
+					}
+				}
+				else if (midFaceY > (120 + 10))
+				{
+					if(servoTiltPosition <= 175)
+					{
+						servoTiltPosition=servoTiltPosition+1;
+					}
+				}
+
+				if(midFaceX < (160 - 10))
+				{
+					if(servoPanPosition >= 5)
+					{
+						servoPanPosition=servoPanPosition-1;
+					}
+				}
+				else if (midFaceX > (160 + 10))
+				{
+					if(servoPanPosition <= 175)
+					{
+						servoPanPosition=servoPanPosition+1;
+					}
+				}
+				writeBuffer[0]=tiltChannel;
+				fwrite(writeBuffer, 1, 1, serPort);
+				printf("tiltChannel = %i", writeBuffer[0]);
+
+				writeBuffer[0]=servoTiltPosition;
+				fwrite(writeBuffer, 1, 1, serPort);
+				printf("servoTiltPosition = %i", writeBuffer[0]);
+
+				writeBuffer[0]=panChannel;
+				fwrite(writeBuffer, 1, 1, serPort);
+				printf("panChannel= %i", writeBuffer[0]);
+
+				writeBuffer[0]=servoPanPosition;
+				fwrite(writeBuffer, 1, 1, serPort);
+				printf("servoPanPosition = %i", writeBuffer[0]);
+*/
+
+				if( nc != lastClass || n == (numBoundingBoxes - 1) )
+				{
+					if( !net->DrawBoxes((float*)imgRGBA, (float*)imgRGBA, camera->GetWidth(), camera->GetHeight(), 
+						                        bbCUDA + (lastStart * 4), (n - lastStart) + 1, lastClass) )
+						printf("detectnet-console:  failed to draw boxes\n");
+						
+					lastClass = nc;
+					lastStart = n;
+
+					CUDA(cudaDeviceSynchronize());
+				}
+			}
+		
+			/*if( font != NULL )
+			{
+				char str[256];
+				sprintf(str, "%05.2f%% %s", confidence * 100.0f, net->GetClassDesc(img_class));
+				
+				font->RenderOverlay((float4*)imgRGBA, (float4*)imgRGBA, camera->GetWidth(), camera->GetHeight(),
+								    str, 10, 10, make_float4(255.0f, 255.0f, 255.0f, 255.0f));
+			}*/
+			
+			if( display != NULL )
+			{
+				char str[256];
+				sprintf(str, "TensorRT build %i.%i.%i | %s | %04.1f FPS", NV_TENSORRT_MAJOR, NV_TENSORRT_MINOR, NV_TENSORRT_PATCH, net->HasFP16() ? "FP16" : "FP32", display->GetFPS());
+				//sprintf(str, "GIE build %x | %s | %04.1f FPS | %05.2f%% %s", NV_GIE_VERSION, net->GetNetworkName(), display->GetFPS(), confidence * 100.0f, net->GetClassDesc(img_class));
+				display->SetTitle(str);	
+			}	
+		}	
+
+		///////////////fclose(serPort);
+		// update display
+		if( display != NULL )
+		{
+			display->UserEvents();
+			display->BeginRender();
+
+			if( texture != NULL )
+			{
+				// rescale image pixel intensities for display
+				CUDA(cudaNormalizeRGBA((float4*)imgRGBA, make_float2(0.0f, 255.0f), 
+								   (float4*)imgRGBA, make_float2(0.0f, 1.0f), 
+		 						   camera->GetWidth(), camera->GetHeight()));
+
+				// map from CUDA to openGL using GL interop
+				void* tex_map = texture->MapCUDA();
+
+				if( tex_map != NULL )
+				{
+					cudaMemcpy(tex_map, imgRGBA, texture->GetSize(), cudaMemcpyDeviceToDevice);
+					texture->Unmap();
+				}
+
+				// draw the texture
+				texture->Render(100,100);		
+			}
+
+			display->EndRender();
+		}
+	}
+	
+	printf("\ndetectnet-camera:  un-initializing video device\n");
+	
+	
+	/*
+	 * shutdown the camera device
+	 */
+	if( camera != NULL )
+	{
+		delete camera;
+		camera = NULL;
+	}
+
+	if( display != NULL )
+	{
+		delete display;
+		display = NULL;
+	}
+	
+	printf("detectnet-camera:  video device has been un-initialized.\n");
+	printf("detectnet-camera:  this concludes the test of the video device.\n");
+	return 0;
+}
+
